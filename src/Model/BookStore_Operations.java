b63d2c4ea3cfe107fe0c8969ec1625ee775b2589
MinhNg1305 <baminh8a1@gmail.com>
2024-04-03 00:24:39 +0800

    fix the BookStore_operation query

--- src/Model/BookStore_Operations.java
+++ src/Model/BookStore_Operations.java
@@ -22,6 +22,7 @@ public class BookStore_Operations {
             String query = "SELECT * FROM orders";
             ResultSet rs = stmt.executeQuery( query );
             int i = 1;
+            int sum = 0;
             while ( rs.next() ) {
                 String order_id = rs.getString( "order_id" );
                 int charge = rs.getInt( "charge" );
@@ -29,14 +30,16 @@ public class BookStore_Operations {
                 String customer_id = rs.getString( "customer_id" );
                 
                 if (o_date.contains(input)) {
+                    sum += charge;
                     System.out.println("Record: " + i);
                     System.out.println("order_id: " + order_id);
-                    System.out.println("customer_id" + customer_id);
+                    System.out.println("customer_id: " + customer_id);
                     System.out.println("date: " + o_date);
                     System.out.println("charge: " + charge);
                     System.out.println();
                     i+=1;
                 }
+                System.out.println("Total charges of the month is " + sum);
             }
         } 
         catch (SQLException e) {
